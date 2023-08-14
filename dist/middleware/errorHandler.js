b95a8c4dc62177ae14b898e4ba9f03da6054f645
Adhis Mauliyahsa Ashafaat <67637463+Ashafaatadhis@users.noreply.github.com>
2023-08-15 05:29:46 +0700

    delete: delete dist directory

--- dist/middleware/errorHandler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-const errorHandler = (err, req, res, next) => {
-    res.status(500).json({ error: "Internal Server Error" });
-};
-exports.errorHandler = errorHandler;
